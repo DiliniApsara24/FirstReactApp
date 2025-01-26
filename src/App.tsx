fefed43195dcd5ee92983419e8f9e3524d90cd39
DiliniApsara24 <diliniapsara605@gmail.com>
2025-01-26 18:05:07 +0530

    Commite

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const App: React.FC = () => {
   return (
     <div>
-      <h1>Hello, React with Dasun!</h1>
+      <h1>This is my First React Project</h1>
       <p>You are the best person</p>
     </div>
   );
